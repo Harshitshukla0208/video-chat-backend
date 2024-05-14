2f6e43482e1e1dda9e7b4acd5f397e8b418fcf56
Harshit Shukla <mrharshit2409@gmail.com>
2024-05-14 21:39:12 +0530

    Room creation logics and storing records of joinees in a temp. array

--- src/handlers/roomHandlers.js
+++ src/handlers/roomHandlers.js
@@ -1,20 +1,35 @@
 const { v4: UUIDv4 } = require("uuid");
+//a temporary memory db
+const rooms = {};
 
 const roomHandler = (socket) => {
-    
     const createRoom = () => {
         const roomId = UUIDv4();
         socket.join(roomId);
+
+        rooms[roomId] = []; //create a new entry for the room
+
         socket.emit("room-created", { roomId });
         console.log("Room created id: ", roomId);
     };
 
-    const joinRoom = () => {
-        console.log("New room joined");
+    const joinedRoom = ({roomId, peerId}) => {
+        if(rooms[roomId]){
+            //if the given room id exist in the mem. db
+            console.log("New user has joined the room", roomId, "with peerID as ", peerId);
+            rooms[roomId].push(peerId);
+            socket.join(roomId) //makes the user join the socket room
+
+            //to check the participants
+            socket.emit("get-users", {
+                roomId: roomId,
+                participants: rooms[roomId]
+            });
+        }
     };
 
     socket.on("create-room", createRoom);
-    socket.on("join-room", joinRoom);
+    socket.on("joined-room", joinedRoom);
 };
 
 module.exports = roomHandler;
